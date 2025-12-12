b751ebe25b9f48868b14d7e6f638c2543d93710b
Console <tiktok_sd@163.com>
2025-12-12 23:01:04 +0800

    feat: consoleMod内部复制一份constant bugfix: 修复当多个同名回调函数同时绑定同一个事件时 除最后一个回调函数之外的所有回调函数失效的问题

--- library/consoleMod/system/client/client.py
+++ library/consoleMod/system/client/client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import mod.client.extraClientApi as clientApi
+from ...constant.clientConstant import *
 
 
 class Main(clientApi.GetClientSystemCls()):
@@ -9,8 +9,29 @@ class Main(clientApi.GetClientSystemCls()):
 
     def InitialListener(self):
         for namespace, systemName, eventName, callback, priority in self.eventList:
-            # 将原来的回调函数同名赋值给自身 否则无法使用
-            # self.原回调函数名 = 原回调函数
-            setattr(self, callback.__name__, callback)
-            # 调用原版接口监听事件
-            self.ListenForEvent(namespace, systemName, eventName, self, getattr(self, callback.__name__), priority)
+            newCallbackName = callback.__name__
+
+            if hasattr(self, newCallbackName):
+                num = 0
+                while True:
+                    num += 1
+                    newCallbackName = callback.__name__ + '_' + str(num)
+                    if not hasattr(self, newCallbackName):
+                        break
+
+            def WrapCallback(func):
+                """
+                [内部函数]生成回调函数包装 防止闭包延迟绑定的问题
+                """
+
+                def wrapper(args):
+                    return func(args)
+
+                wrapper.func_name = newCallbackName
+                return wrapper
+
+            callbackWrapper = WrapCallback(callback)
+            # 设置属性
+            setattr(self, newCallbackName, callbackWrapper)
+            # 调用原版监听接口
+            self.ListenForEvent(namespace, systemName, eventName, self, getattr(self, newCallbackName), priority)
