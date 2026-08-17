98af0cb4270c2a473844ef4f4ae6f35cfb8ad86f
Console <tiktok_sd@163.com>
2026-08-18 01:22:34 +0800

    fix: 补删客户端InitialListener残留代码

--- library/consoleMod/system/client/client.py
+++ library/consoleMod/system/client/client.py
@@ -19,32 +19,3 @@ class Main(clientApi.GetClientSystemCls()):
     def Destroy():
         for func in destroyFunc:
             func()
-
-    def InitialListener(self):
-        for namespace, systemName, eventName, callback, priority in self.eventList:
-            newCallbackName = callback.__name__
-
-            if hasattr(self, newCallbackName):
-                num = 0
-                while True:
-                    num += 1
-                    newCallbackName = callback.__name__ + '_' + str(num)
-                    if not hasattr(self, newCallbackName):
-                        break
-
-            def WrapCallback(func):
-                """
-                [内部函数]生成回调函数包装 防止闭包延迟绑定的问题
-                """
-
-                def wrapper(args):
-                    return func(args)
-
-                wrapper.func_name = newCallbackName
-                return wrapper
-
-            callbackWrapper = WrapCallback(callback)
-            # 设置属性
-            setattr(self, newCallbackName, callbackWrapper)
-            # 调用原版监听接口
-            self.ListenForEvent(namespace, systemName, eventName, self, getattr(self, newCallbackName), priority)
